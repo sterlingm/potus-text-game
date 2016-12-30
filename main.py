c8b19825c59c0bf0db616555f6cd08a068daa531
Sterling McLeod <sterlingmcleod9@gmail.com>
2016-12-30 16:40:38 -0500

    Added some introductory strings to print at the beginning

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import character
 import room
 import time
+import sys
 import os
 
 
@@ -46,19 +47,14 @@ def main():
     char = character.Character("sterling", rooms['Entrance'])
     begin(char, rooms)
 
-def begin(char, rooms):
-    os.system('reset')
-    str_wel = "\nWelcome %s!" % char.name
-    str_intro = '''
-        ######################################################
-        ######################################################
-        ######################################################
-        
-        Welcome %s!\n
-        You are about to play POTUS!
-        Your goal: Find the President and stop him from pushing
-        the\n\n
-    '''
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write('%s' % c)
+        sys.stdout.flush()
+        time.sleep(0.025)
+
+def print_brb():
     str_big = '''
 
 
@@ -98,14 +94,90 @@ def begin(char, rooms):
               _\\/////////////_______\\/////////___________\\///______________\\///_____________\\/////_______\\///_____\\/////__
               '''
 
-    print str_wel
-    print str_intro
     print '\033[00;31m' + str_big
     time.sleep(1)
     print str_red
     time.sleep(1)
     print str_button
-    #char.print_info()
+
+    print '\033[00;37m'
+
+
+def begin(char, rooms):
+    os.system('reset')
+    str_wel = "\nWelcome %s!" % char.name
+    str_intro = '''
+        ######################################################
+        ######################################################
+        ######################################################
+        
+        Welcome %s!\n
+        You are about to play POTUS!
+        Your goal: Find the President and stop him from pushing
+        the\n\n
+    '''
+
+    str_loc = '''
+    Your location is the entrance to the West Wing of the White House.
+    This part of the White House is home to the Oval Office, the Cabinet Room, 
+    and other offices of the Executive Branch.
+    The President roams these halls throughout the day, discussing various 
+    matters with his staff, and letting everybody know how cool he is.
+    
+    '''
+
+    str_rules = '''
+    Each room you enter may contain several employees. An employee may have 
+    useful information for you, such as rumors or clues about the President's 
+    location, or they could have no information. They could also want to talk to
+    you and figure out what you're up to. Most employees will have certain 
+    strengths and weaknesses to the various interactions you can make with them,
+    such as small talk.
+
+    Interacting with an employee takes time so make sure you don't get caught up
+    talking to 1 employee for too long! Do your best to get information from the
+    useful employees, and get past the useless ones as fast as possible!
+
+    '''
+    str_story = '''
+    Today, a phone call from the China-Mexico-Muslim Alliance was received.  
+    According to your sources, the Alliance wanted to ensure that the President 
+    saw their latest Tweet that read "Murica don't do nuttin cept lose to us in
+    errthang, fatties". The POTUS was heard furiously screaming throughout the 
+    entire West Wing. The entire Twitter department has been fired, and now the 
+    President feels that his only choice is to press the big red button. Nobody 
+    knows what the big red button does, but we're pretty sure it nukes people. 
+    
+    '''
+
+    str_task = '''
+    Your task is to find the President and calm him down. Explore the West Wing,
+    gather information from the people in the offices about the President's 
+    location, and make sure to find him FAST before he does anything 
+    irreversible! It is up to YOU to stop him from pushing the
+
+    '''
+
+    print str_wel
+    print str_intro
+    #delay_print(str_wel)
+    #delay_print(str_intro)
+    print_brb()
+
+    time.sleep(1.0)
+
+    delay_print(str_story)
+    delay_print(str_task)
+    #print str_loc
+    #print str_story
+    #print str_task
+    
+    time.sleep(0.2)
+    print_brb()
+
+    delay_print(str_loc)
+    delay_print(str_rules)
+
 
     str_prompt = "\nChoose an action"
     str_actions = "\nMove, Interact"
